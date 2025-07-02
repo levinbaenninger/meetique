3e1bfbca1179f099f63f87cc1893bc34f35a43c1
Levin Bänninger <l.baenninger@icloud.com>
2025-07-02 15:42:34 +0200

    feat: set url dynamically (#11)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,14 +3,17 @@ import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 
 import { db } from '@/db';
 import * as schema from '@/db/schema';
+import { BETTER_AUTH_URL } from '@/lib/env';
 
 export const auth = betterAuth({
+  baseURL: BETTER_AUTH_URL,
   database: drizzleAdapter(db, {
     provider: 'pg',
     schema: {
       ...schema,
     },
   }),
+  trustedOrigins: [BETTER_AUTH_URL],
   emailAndPassword: {
     enabled: true,
   },

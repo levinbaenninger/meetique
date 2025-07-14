2d1486a2dad79e687b3df0d15a86e8f15b3f70a3
Levin Bänninger <l.baenninger@icloud.com>
2025-07-14 08:34:52 +0200

    chore: remove sendDefaultPii property to align with compliance

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -7,8 +7,6 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || '',
 
-  sendDefaultPii: true,
-
   // Add optional integrations for additional features
   integrations: [
     Sentry.replayIntegration(),

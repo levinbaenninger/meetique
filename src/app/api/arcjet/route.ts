16b4ee759b3ade6124cb3f26ff73026de7b1d681
Levin Bänninger <l.baenninger@icloud.com>
2025-07-14 15:23:23 +0200

    chore: set up Arcjet

---
+++ src/app/api/arcjet/route.ts
@@ -0,0 +1,54 @@
+import { isSpoofedBot } from '@arcjet/inspect';
+import arcjet, { detectBot, shield, tokenBucket } from '@arcjet/next';
+import { NextResponse } from 'next/server';
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  characteristics: ['ip.src'],
+  rules: [
+    shield({ mode: 'LIVE' }),
+    detectBot({
+      mode: 'LIVE',
+      allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:MONITOR', 'CATEGORY:PREVIEW'],
+    }),
+    tokenBucket({
+      mode: 'LIVE',
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
+
+export async function GET(req: Request) {
+  const decision = await aj.protect(req, { requested: 5 });
+  console.log('Arcjet decision', decision);
+
+  if (decision.isDenied()) {
+    if (decision.reason.isRateLimit()) {
+      return NextResponse.json(
+        { error: 'Too Many Requests', reason: decision.reason },
+        { status: 429 },
+      );
+    } else if (decision.reason.isBot()) {
+      return NextResponse.json(
+        { error: 'No bots allowed', reason: decision.reason },
+        { status: 403 },
+      );
+    } else {
+      return NextResponse.json(
+        { error: 'Forbidden', reason: decision.reason },
+        { status: 403 },
+      );
+    }
+  }
+
+  if (decision.results.some(isSpoofedBot)) {
+    return NextResponse.json(
+      { error: 'Forbidden', reason: decision.reason },
+      { status: 403 },
+    );
+  }
+
+  return NextResponse.json({ message: 'Hello world' });
+}

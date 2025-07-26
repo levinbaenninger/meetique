8d8d2103540d8c1db68b643806606717676440ef
Levin Bänninger <l.baenninger@icloud.com>
2025-07-26 22:50:30 +0200

    feat: set up magic link auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,13 @@
 import { checkout, polar, portal } from '@polar-sh/better-auth';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { magicLink } from 'better-auth/plugins';
 
 import { db } from '@/db';
 import * as schema from '@/db/schema';
 import { BETTER_AUTH_URL, TRUSTED_ORIGINS } from '@/lib/env';
-
-import { polarClient } from './polar';
+import { resend } from '@/lib/mail';
+import { polarClient } from '@/lib/polar';
 
 export const auth = betterAuth({
   baseURL: BETTER_AUTH_URL,
@@ -17,9 +18,6 @@ export const auth = betterAuth({
     },
   }),
   trustedOrigins: TRUSTED_ORIGINS,
-  emailAndPassword: {
-    enabled: true,
-  },
   socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
@@ -31,6 +29,16 @@ export const auth = betterAuth({
     },
   },
   plugins: [
+    magicLink({
+      sendMagicLink: async ({ email, url }) => {
+        await resend.emails.send({
+          from: 'meetique@levinbaenninger.dev',
+          to: email,
+          subject: 'Your magic link',
+          text: `Click the link to securely sign in to your account: ${url}`,
+        });
+      },
+    }),
     polar({
       client: polarClient,
       createCustomerOnSignUp: true,
